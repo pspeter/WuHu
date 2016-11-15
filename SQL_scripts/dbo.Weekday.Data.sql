d83dd712fef83c7a87e506cbe95340cd6d95e317
bloody-orange <peter.schmidb@gmail.com>
2016-11-15 21:42:30 +0100

    Some more partly generated SQL inserts

---
+++ SQL_scripts/dbo.Weekday.Data.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO [dbo].[Weekday] ([Day]) VALUES ('Montag')
+INSERT INTO [dbo].[Weekday] ([Day]) VALUES ('Dienstag')
+INSERT INTO [dbo].[Weekday] ([Day]) VALUES ('Mittwoch')
+INSERT INTO [dbo].[Weekday] ([Day]) VALUES ('Donnerstag')
+INSERT INTO [dbo].[Weekday] ([Day]) VALUES ('Freitag')
+INSERT INTO [dbo].[Weekday] ([Day]) VALUES ('Samstag')
+INSERT INTO [dbo].[Weekday] ([Day]) VALUES ('Sonntag')
