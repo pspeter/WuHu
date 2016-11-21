171d88b5c4b712f916ef8ba0996f9420b2413293
Peter Schmidbauer <peter.schmidb@gmail.com>
2016-11-21 13:13:57 +0100

    Made Tournament pk also nullable.

--- WuHu/WuHu.Domain/Tournament.cs
+++ WuHu/WuHu.Domain/Tournament.cs
@@ -12,14 +12,14 @@ namespace WuHu.Domain
 
         public Tournament() { }
 
-        public Tournament(int tournamentId, string name, int creatorId)
+        public Tournament(int? tournamentId, string name, int creatorId)
         {
             this.TournamentId = tournamentId;
             this.Name = name;
             this.Creator = creatorId;
         }
        
-        public int TournamentId { get; set; }
+        public int? TournamentId { get; set; }
         public string Name { get;  set; }
         public int Creator { get; set; }
 
