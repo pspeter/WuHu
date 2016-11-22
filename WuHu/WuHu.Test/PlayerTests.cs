7d71fa70e2da45ec994cf7ca9b06d3857055cb36
bloody-orange <peter.schmidb@gmail.com>
2016-11-22 23:05:26 +0100

    added some player tests. The database gets rebuilt before and after testing. (once per class)

--- WuHu/WuHu.Test/PlayerTests.cs
+++ WuHu/WuHu.Test/PlayerTests.cs
@@ -5,6 +5,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Configuration;
 using WuHu.Dal.Common;
 using WuHu.Domain;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace WuHu.Test
 {
@@ -22,6 +24,8 @@ namespace WuHu.Test
         {
             connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
             database = DalFactory.CreateDatabase();
+            database.DropTables();
+            database.CreateTables();
             PlayerTestContext = testContext;
             playerDao = DalFactory.CreatePlayerDao(database);
         }
@@ -29,13 +33,13 @@ namespace WuHu.Test
         [ClassCleanup]
         public static void ClassCleanup()
         {
-
+            database.DropTables();
+            database.CreateTables();
         }
 
         [TestInitialize]
         public void TestInitialize()
         {
-
         }
 
         [TestCleanup]
@@ -43,41 +47,103 @@ namespace WuHu.Test
         {
 
         }
+
         
 
         [TestMethod]
         public void Insert()
         {
+            //generates a random string for our user field
+            string uniqueUsername = Guid.NewGuid().ToString().Substring(0, 15);
             int cnt = playerDao.Count();
-            Assert.AreEqual(0, cnt);
-            playerDao.Insert(new Player("first", "last", "nick", "user", "pass", 
+            var playerId = playerDao.Insert(new Player("first", "last", "nick", uniqueUsername, "pass", 
                 false, false, false, false, false, true, true, true, null));
             int newCnt = playerDao.Count();
             Assert.AreEqual(cnt + 1, newCnt);
+            Assert.IsInstanceOfType(playerId, typeof(int));
+            Assert.IsTrue(playerId >= 0);
         }
 
         [TestMethod]
-        public void InsertNicknameUnique()
+        public void InsertUserUniqueConstraint()
         {
-            throw new NotImplementedException();
+            string uniqueUsername = Guid.NewGuid().ToString().Substring(0, 15);
+            Player player = new Player("", "", "", uniqueUsername, "", false, false, 
+                                false, false, false, false, false, false, null);
+
+            playerDao.Insert(player);
+            try
+            {
+                playerDao.Insert(player);
+                Assert.Fail("No SqlException thrown.");
+            }
+            catch (SqlException)
+            { }
         }
 
         [TestMethod]
         public void Count()
         {
+            int cnt1 = playerDao.Count();
+            Assert.IsTrue(cnt1 >= 0);
+            int cnt2 = playerDao.Count();
+            Assert.AreEqual(cnt1, cnt2);
 
+            string uniqueUsername = Guid.NewGuid().ToString().Substring(0, 15);
+            playerDao.Insert(new Player("first", "last", "nick", uniqueUsername, "pass",
+                false, false, false, false, false, true, true, true, null));
+
+            int cnt3 = playerDao.Count();
+            Assert.AreEqual(cnt1 + 1, cnt3);
         }
 
         [TestMethod]
         public void Update()
         {
+            string uniqueUsername = Guid.NewGuid().ToString().Substring(0, 15);
+            Player player = new Player("first", "last", "nick", uniqueUsername, "pass",
+                false, false, false, false, false, true, true, true, null);
 
+            int playerId = playerDao.Insert(player);
+
+            string newFirst = "newFirst";
+            player.PlayerId = playerId;
+            player.FirstName = newFirst;
+            playerDao.Update(player);
+
+            player = playerDao.FindById(playerId);
+            Assert.AreEqual(newFirst, player.FirstName);
+        }
+
+        [TestMethod]
+        public void UpdateWithoutPlayerIdFails()
+        {
+            string uniqueUsername = Guid.NewGuid().ToString().Substring(0, 15);
+            Player player = new Player("first", "last", "nick", uniqueUsername, "pass",
+                false, false, false, false, false, true, true, true, null);
+            try
+            {
+                playerDao.Update(player);
+                Assert.Fail();
+            }
+            catch (ArgumentException)
+            { }
         }
 
         [TestMethod]
         public void Constructor()
         {
+            Player player1 = new Player();
 
+            string uniqueUsername = Guid.NewGuid().ToString().Substring(0, 15);
+            Player player2 = new Player("first", "last", "nick", uniqueUsername, "pass",
+                false, false, false, false, false, true, true, true, null);
+
+            uniqueUsername = Guid.NewGuid().ToString().Substring(0, 15);
+            byte[] pw = new byte[32];
+            byte[] salt = new byte[32];
+            Player player3 = new Player(0, "first", "last", "nick", uniqueUsername, pw, salt,
+                false, false, false, false, false, true, true, true, null);
         }
 
        
