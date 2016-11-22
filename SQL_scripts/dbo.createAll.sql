03d465871306c101aba56feb4e538652f7e635a3
bloody-orange <peter.schmidb@gmail.com>
2016-11-22 23:02:45 +0100

    changed createAll script to proper sql syntax. Added droptable script

--- SQL_scripts/dbo.createAll.sql
+++ SQL_scripts/dbo.createAll.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE TABLE [dbo].[Match]
+﻿CREATE TABLE [dbo].[Match]
 ( 
 	[tournamentId]       int  NOT NULL,
 	[matchId]            int  NOT NULL  IDENTITY ( 0,1 ),
@@ -12,20 +11,18 @@ CREATE TABLE [dbo].[Match]
 	[scoreTeam2]         TINYINT  NULL ,
 	[estimatedWinChance]       FLOAT  NOT NULL ,
 	[isDone]             bit  NOT NULL 
-)
-go
+);
 
 ALTER TABLE [dbo].[Match]
-	ADD CONSTRAINT [XPKMatch] PRIMARY KEY  CLUSTERED ([matchId] ASC)
-go
+	ADD CONSTRAINT [XPKMatch] PRIMARY KEY  CLUSTERED ([matchId] ASC);
 
 CREATE TABLE [dbo].[Player]
 ( 
 	[playerId]           int  NOT NULL  IDENTITY ( 0,1 ) ,
-	[firstName]          nchar(100)  NOT NULL ,
-	[lastName]           nchar(100)  NOT NULL ,
-	[nickName]           nchar(20)  NULL ,
-	[userName]           nchar(20)  UNIQUE NOT NULL ,
+	[firstName]          nvarchar(100)  NOT NULL ,
+	[lastName]           nvarchar(100)  NOT NULL ,
+	[nickName]           nvarchar(20)  NULL ,
+	[userName]           nvarchar(20)  UNIQUE NOT NULL ,
 	[password]           VARBINARY(32)  NOT NULL ,
 	[salt]               VARBINARY(32)   NOT NULL ,
 	[picture]            VARBINARY(5000)  NULL ,
@@ -37,12 +34,10 @@ CREATE TABLE [dbo].[Player]
     [playsFridays]       bit    NOT NULL,
     [playsSaturdays]     bit    NOT NULL,
     [playsSundays]       bit    NOT NULL
-)
-go
+);
 
 ALTER TABLE [dbo].[Player]
-	ADD CONSTRAINT [XPKPlayer] PRIMARY KEY  CLUSTERED ([playerId] ASC)
-go
+	ADD CONSTRAINT [XPKPlayer] PRIMARY KEY  CLUSTERED ([playerId] ASC);
 
 CREATE TABLE [dbo].[Rating]
 ( 
@@ -52,78 +47,65 @@ CREATE TABLE [dbo].[Rating]
 	CONSTRAINT [now_timestamp]
 		 DEFAULT  CURRENT_TIMESTAMP,
 	[value]              int  NULL 
-)
-go
+);
 
 ALTER TABLE [dbo].[Rating]
-	ADD CONSTRAINT [XPKRating] PRIMARY KEY  CLUSTERED ([ratingId] ASC)
-go
+	ADD CONSTRAINT [XPKRating] PRIMARY KEY  CLUSTERED ([ratingId] ASC);
 
 CREATE TABLE [dbo].[ScoreParameter]
 ( 
-	[key]                nchar(200)  NOT NULL,
-	[value]               nchar(200)  NOT NULL 
-)
-go
+	[key]                nvarchar(200)  NOT NULL,
+	[value]               nvarchar(200)  NOT NULL 
+);
 
 ALTER TABLE [dbo].[ScoreParameter]
-	ADD CONSTRAINT [XPKScoreParameter] PRIMARY KEY  CLUSTERED ([key] ASC)
-go
+	ADD CONSTRAINT [XPKScoreParameter] PRIMARY KEY  CLUSTERED ([key] ASC);
 
 CREATE TABLE [dbo].[Tournament]
 ( 
 	[tournamentId]       int  NOT NULL  IDENTITY ( 0,1 ) ,
-	[name]               nchar(50)  NOT NULL ,
+	[name]               nvarchar(50)  NOT NULL ,
 	[creator]           int  NULL 
-)
-go
+);
 
 ALTER TABLE [dbo].[Tournament]
-	ADD CONSTRAINT [XPKTournament] PRIMARY KEY  CLUSTERED ([tournamentId] ASC)
-go
+	ADD CONSTRAINT [XPKTournament] PRIMARY KEY  CLUSTERED ([tournamentId] ASC);
 
 ALTER TABLE [dbo].[Rating]
 	ADD CONSTRAINT [R_1] FOREIGN KEY ([playerId]) REFERENCES [Player]([playerId])
 		ON DELETE CASCADE
-		ON UPDATE CASCADE
-go
+		ON UPDATE CASCADE;
 
 ALTER TABLE [dbo].[Match]
 	ADD CONSTRAINT [R_9] FOREIGN KEY ([tournamentId]) REFERENCES [Tournament]([tournamentId])
 		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
-go
+		ON UPDATE NO ACTION;
 
 
 
 ALTER TABLE [dbo].[Match]
 	ADD CONSTRAINT [R_10] FOREIGN KEY ([player1]) REFERENCES [Player]([playerId])
 		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
-go
+		ON UPDATE NO ACTION;
 
 ALTER TABLE [dbo].[Match]
 	ADD CONSTRAINT [R_11] FOREIGN KEY ([player2]) REFERENCES [Player]([playerId])
 		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
-go
+		ON UPDATE NO ACTION;
 
 ALTER TABLE [dbo].[Match]
 	ADD CONSTRAINT [R_12] FOREIGN KEY ([player3]) REFERENCES [Player]([playerId])
 		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
-go
+		ON UPDATE NO ACTION;
 
 ALTER TABLE [dbo].[Match]
 	ADD CONSTRAINT [R_13] FOREIGN KEY ([player4]) REFERENCES [Player]([playerId])
 		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
-go
+		ON UPDATE NO ACTION;
 
 
 ALTER TABLE [dbo].[Tournament]
 	ADD CONSTRAINT [R_2] FOREIGN KEY ([creator]) REFERENCES [Player]([playerId])
 		ON DELETE CASCADE
-		ON UPDATE CASCADE
-go
+		ON UPDATE CASCADE;
 
---
+++ SQL_scripts/dbo.dropAll.sql
@@ -0,0 +1,5 @@
+DROP TABLE dbo.Match;
+DROP TABLE dbo.Tournament;
+DROP TABLE dbo.Rating;
+DROP TABLE dbo.Player;
+DROP TABLE dbo.ScoreParameter;
