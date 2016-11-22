cb4d3c19a37df25c9da32c95c4a23a3d78c91a4b
bloody-orange <peter.schmidb@gmail.com>
2016-11-22 23:28:26 +0100

    only drop tables if they exist

--- SQL_scripts/dbo.dropAll.sql
+++ SQL_scripts/dbo.dropAll.sql
@@ -1,5 +1,10 @@
-DROP TABLE dbo.Match;
-DROP TABLE dbo.Tournament;
-DROP TABLE dbo.Rating;
-DROP TABLE dbo.Player;
-DROP TABLE dbo.ScoreParameter;
+IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE  TABLE_NAME = 'Player'))
+BEGIN
+    DROP TABLE dbo.Match;
+    DROP TABLE dbo.Tournament;
+    DROP TABLE dbo.Rating;
+    DROP TABLE dbo.Player;
+    DROP TABLE dbo.ScoreParameter;
+END;
