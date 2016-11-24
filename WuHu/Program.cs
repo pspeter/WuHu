ae3f9506ad69232f31610bf49c1bdcc398c0463f
Peter Schmidbauer <peter.schmidb@gmail.com>
2016-11-24 04:34:08 +0100

    Program.cs now sets up the testdata

--- WuHu/Program.cs
+++ WuHu/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WuHu.Dal.Common;
+using WuHu.Dal.SqlServer;
+using WuHu.Test;
 
 namespace WuHu.Server
 {
@@ -10,6 +14,9 @@ namespace WuHu.Server
     {
         static void Main(string[] args)
         {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+            IDatabase database = DalFactory.CreateDatabase();
+            CommonData.InsertTestData(database);
         }
     }
 }
